0e67a6de0face6e9d688e806dc50cfdbd695b603
samoina mbuguah <samoina.mbuguah@student.moringaschool.com>
2021-04-14 21:27:43 +0300

    Add classes to HTML and update JS to include toggle effects

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,11 +1,22 @@
-jQuery("hi").click(function() {
-  alert("This is a heading tag")
-});
+// jQuery(document).ready(function() {
+//   jQuery("h1").click(function() {
+//     alert("This is a heading tag")
+//     alert("I have just said this is a heading tag")
+//   });
+  
+//   jQuery("p").click(function() {
+//     alert("This is a paragraph")
+//   });
+  
+//   jQuery("img").click(function() {
+//     alert("This is an image")
+//   });
+  
+// })
 
-jQuery("p").click(function() {
-  alert("This is a paragraph")
-});
-
-jQuery("img").click(function() {
-  alert("This is an image")
+$(document).ready(function() {
+  $("p").click(function() {
+    $(".walrus-showing").toggle();
+    $(".walrus-hidden").toggle();
+  });
 });
